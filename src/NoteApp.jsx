2012752cc034e395872b8f1c4b64e71abba8d0d3
Amir Hossein Kargar <amirkaargar@hotmail.com>
2024-10-27 22:39:51 +0330

    feat(body): Implemented state to manage input value.

--- src/NoteApp.jsx
+++ src/NoteApp.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
+
 function NoteApp() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    const newNote = {
+      title,
+      description,
+      id: Date.now(),
+      completed: false,
+      createdAt: new Date().toISOString()
+    };
+    setTitle("")
+    setDescription("")
+    console.log(newNote);
+    
+    
+  };
+
   return (
     <div className="note-app flex justify-between gap-8">
       <div className="add-new-note w-4/12 text-center">
         <h2 className="mb-10 text-2xl font-bold tracking-wide">Add New Note</h2>
-        <form action="" className="gap-6 grid">
+        <form action="" className="gap-6 grid" onSubmit={handleSubmit}>
           <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             type="text"
             placeholder="Note Title"
             className="text-center rounded-lg shadow-sm p-3 focus:outline-none focus:ring-2 focus:ring-blue-500 text-main-color bg-text-color w-full"
           />
           <input
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
             type="text"
             placeholder="Note Description"
             className="text-center rounded-lg shadow-sm p-3 focus:outline-none focus:ring-2 focus:ring-blue-500 text-main-color bg-text-color w-full"
