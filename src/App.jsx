df0f756b799bb89dc57c865e7647f1a7a3cc1f49
Amir Hossein Kargar <amirkaargar@hotmail.com>
2025-01-25 23:55:27 +0330

    feat: implement note addition, deletion, and completion using useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,59 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import Swal from "sweetalert2";
 
 import NoteHeader from "./NoteHeader";
 import AddNewNote from "./AddNewNote";
 import NoteList from "./NoteList";
-function App() {
-  // eslint-disable-next-line no-undef, no-unused-vars
-  const [notes, setNotes] = useState([]);
+
+function notesReducer(state, action) {
+switch(action.type) {
+  case "add": {
+    return [...state, action.payload];
+  } case "delete" : {
+   return state.filter(s => s.id !== action.payload)
+  } case "complete" : {
+   return state.map(note => note.id == action.payload ? {...note, completed: !note.completed} : note)
+  }
+  default : throw new Error("Unknown error" + action.type)
+}  
+}
+
+export default function App() {  
+ const [notes, dispatch] = useReducer(notesReducer, [])
 
   const handleAddNotes = (newNote) => {
-    setNotes((prevNotes) => [...prevNotes, newNote]);
+    dispatch({type: "add", payload: newNote})
   };
 
   const handleDeleteNote = (id) => {
-    Swal.fire({
-      title: "Are you sure you want to delete this note?",
-      text: "This action cannot be undone!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#ef4444",
-      cancelButtonColor: "#3b82f6",
-      confirmButtonText: "Yes, delete it!",
-      cancelButtonText: "Cancel",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
-        Swal.fire("Deleted!", "Your note has been deleted.", "success");
-      }
-    });
+    // Swal.fire({
+    //   title: "Are you sure you want to delete this note?",
+    //   text: "This action cannot be undone!",
+    //   icon: "warning",
+    //   showCancelButton: true,
+    //   confirmButtonColor: "#ef4444",
+    //   cancelButtonColor: "#3b82f6",
+    //   confirmButtonText: "Yes, delete it!",
+    //   cancelButtonText: "Cancel",
+    // }).then((result) => {
+    //   if (result.isConfirmed) {
+    //     setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+    //     Swal.fire("Deleted!", "Your note has been deleted.", "success");
+    //   }
+    // });
+    dispatch({type: 'delete', payload: id})
   };
 
   const handleCompleteNotes = (e) => {
     const noteId = Number(e.target.value)
-    setNotes(prevNotes => prevNotes.map(note => note.id == noteId ? {...note, completed: !note.completed} : note))
+    dispatch({type: "complete", payload: noteId})
   };
 
   return (
-    <div className="container mx-auto max-w-screen-xl my-8 p-4">
-      <NoteHeader />
-      <div className="note-app flex flex-col gap-32 justify-center items-center sm:flex-row">
+    <div className="container max-w-screen-xl p-4 mx-auto my-8">
+      <NoteHeader  notes={notes}/>
+      <div className="flex flex-col items-center justify-center gap-32 note-app sm:flex-row">
         <AddNewNote onAddNote={handleAddNotes} />
         <NoteList
           notes={notes}
@@ -50,5 +64,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
--- src/NoteList.jsx
+++ src/NoteList.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 function NoteList({ notes, onDelete, onComplete }) {
   return (
-    <div className="w-2/3 text-center min-h-screen">
+    <div className="w-2/3 min-h-screen text-center">
       <div>
         <h2 className="text-lg tracking-wide">
           {/* No notes have been added. <span>🤔</span> */}
@@ -33,20 +33,20 @@ function NoteIem({ note, onDelete, onComplete }) {
 
   return (
     <div className={`note-item p-6 bg-text-color rounded-2xl mb-6`}>
-      <div className="note-item__header flex items-center justify-between pb-4 mb-8 border-b border-black">
+      <div className="flex items-center justify-between pb-4 mb-8 border-b border-black note-item__header">
         <div className={`tracking-wide text-stone-700 ${note.completed ? "line-through opacity-70" : ""}`}>
-          <h2 className="title text-text-700 text-2xl mb-2 font-bold text-left">
+          <h2 className="mb-2 text-2xl font-bold text-left title text-text-700">
             {note.title}
           </h2>
-          <h4 className="desc text-xl font-normal text-text-400 text-left">
+          <h4 className="text-xl font-normal text-left desc text-text-400">
             {note.description}
           </h4>
         </div>
-        <div className="actions flex gap-4 items-center">
+        <div className="flex items-center gap-4 actions">
           <button onClick={() => onDelete(note.id)}>❌</button>
           <input
             type="checkbox"
-            className="transform scale-150 h-10"
+            className="h-10 transform scale-150"
             name={note.id}
             id={note.id}
             value={note.id}
@@ -55,8 +55,8 @@ function NoteIem({ note, onDelete, onComplete }) {
           />
         </div>
       </div>
-      <div className="note-item__footer w-3/6">
-        <span className="text-text-400 text-left block cursor-default select-none whitespace-nowrap">
+      <div className="w-3/6 note-item__footer">
+        <span className="block text-left cursor-default select-none text-text-400 whitespace-nowrap">
           {new Date(note.createdAt).toLocaleDateString("en-US", options)}
         </span>
       </div>
